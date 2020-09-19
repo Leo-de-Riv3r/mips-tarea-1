930e48bb86a69656d2ed71a1e0dd0417ba94e60a
Leo-de-Riv3r <leoguti182@gmail.com>
2020-09-19 16:39:01 -0300

    Update 2_factorial.asm

--- 2_factorial.asm
+++ 2_factorial.asm
@@ -6,28 +6,28 @@ msg: .asciiz "El factorial es "
 .text
 .globl main
 main:
-	li	$v0, 4 # print_string code
+	li	$v0, 4           # print_string code
 	la	$a0, prompt  
 	syscall # puts(prompt);
 	li	$v0, 5
 	syscall # scanf("%i", &num);
-	move 	$t0, $v0 # int num;
-	li      $t1, 1 # result = 1;
+	move 	$t0, $v0         # int num;
+	li      $t1, 1           # result = 1;
 	li	$t2, 1 
 loop:   
-        mul $t1, $t1, $t0 # result *= num;
-        addi $t0, $t0, -1 # num--;
-        bgt $t0, $t2, loop # if (num > 1) goto loop;
+        mul     $t1, $t1, $t0    # result *= num;
+        addi    $t0, $t0, -1     # num--;
+        bgt     $t0, $t2, loop   # if (num > 1) goto loop;
 		
 exit:
 	li      $v0, 4
 	la	$a0, new_line
 	syscall
 	li	$v0, 4
-	la	$a0, msg
+	la	$a0, msg 
 	syscall
 	li	$v0, 1
 	move	$a0, $t1
-	syscall # printf("%i", result);
+	syscall                   # printf("%i", result);
 	li	$v0, 10
 	syscall 
